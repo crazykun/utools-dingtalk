0333c1d02b4aa8d2d14c8e987314cf80bc7d008e
crazykun <jilikun@gmail.com>
2020-08-07 16:55:01 +0800

    消息提醒优化

--- README.md
+++ README.md
@@ -3,4 +3,23 @@
 > 这是给 Linux 的同学准备的
 
 
+### uTools链接
+官网 [uTools](https://https://u.tools/)
+
+社区 [uTools社区](https://yuanliao.info/)
+
+
+### 安装方法
+安装插件后，在输入框输入 “钉钉”
+
+
+### 使用方法
+通过关键字打开, 关键词:
+
+* 钉钉
+* 钉
+* dd
+* dingding
+* dingTalk
+
 
--- preload.js
+++ preload.js
@@ -7,64 +7,70 @@ let unreadTotal = 0
  * 初始化 web dingtalk
  */
 function dingTalkInit() {
-    console.log(unreadTotal)
-    console.log(browser_id)
-    if (!browser_id) {
-        let browsers = utools.getIdleUBrowsers()
-        if (browsers && browsers.length > 0) {
-            browser_id = browsers[0].id
-        }
-    }
-    // 打开旧窗口
     if (browser_id) {
-        console.log("open ubroser")
-        utools.ubrowser
-            .show()
-            .devTools('bottom')
-            .run(browser_id)
+        dingTalkShow(browser_id)
     } else {
-        console.log("new ubroser")
         utools.ubrowser.goto('https://im.dingtalk.com/')
-            .devTools('bottom')
-            .evaluate((unreadTotal) => {
-                setInterval(() => {
-                    let el =
-                        document &&
-                        document.querySelector('.main-menus .unread-num em.ng-binding')
-                    let total = 0
-                    if (el) {
-                        total = ~~el.textContent.trim()
-                    }
-                    if (total !== unreadTotal && total > 0) {
-                        console.log("new message")
-                        // utools.showNotification('您有 ${total} 条钉钉信息')
-                        const notify = new Notification('钉钉', {
-                            icon: 'https://g.alicdn.com/dingding/web/0.2.6/img/oldIcon.ico',
-                            body: `您有 ${total}  条钉钉信息`
-                        })
-                        notify.onclick = () => {
-                            dingTalkInit()
-                        }
-                        
-                    }
-                    unreadTotal = total
-                }, 1000)
-            }, unreadTotal)
+            .devTools('right')
+            .evaluate(dingTalkNotice, unreadTotal)
             .run({ width: 1000, height: 600 })
     }
 }
 
-// utools.showNotification('您有 ${total} 条钉钉信息')
+/**
+ *  钉钉消息提醒
+ */
+function dingTalkNotice(unreadTotal) {
+    console.log(unreadTotal)
+    setInterval(() => {
+        let el =
+            document &&
+            document.querySelector('.main-menus .unread-num em.ng-binding')
+        let total = 0
+        if (el) {
+            total = ~~el.textContent.trim()
+        }
+        if (total > unreadTotal) {
+            console.log("new message")
+            // utools.showNotification('您有 ${total} 条钉钉信息')
+            const notify = new Notification('钉钉', {
+                icon: 'https://g.alicdn.com/dingding/web/0.2.6/img/newIcon.ico',
+                body: `您有 ${total}  条钉钉信息`
+            })
+            notify.onclick = () => {
+                window.focus();
+                // utools.ubrowser.show()
+            }
+
+        }
+        unreadTotal = total
+    }, 1000)
+}
+
+/**
+ *  打开钉钉窗口
+ */
+function dingTalkShow(browser_id) {
+    utools.ubrowser
+        .show()
+        .devTools('right')
+        .run(browser_id)
+}
+
 
 utools.onPluginReady(() => {
     console.log('插件装配完成，已准备好')
-    // utools.hideMainWindow()
+    utools.hideMainWindow()
 })
 
 
 utools.onPluginEnter(({ code, type, payload, optional }) => {
     console.log('用户进入插件', code, type, payload)
     dingTalkInit()
+    let browsers = utools.getIdleUBrowsers()
+    if (browsers && browsers.length > 0) {
+        browser_id = browsers[0].id
+    }
 })
 
 
